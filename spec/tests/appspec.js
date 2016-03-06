0a36d2b0a14c534e1d27629ddf8316f1d7971bea
kes <glkes@yahoo.com>
2016-03-06 14:42:16 -0500

    ignore and test

--- spec/tests/appspec.js
+++ spec/tests/appspec.js
@@ -1,7 +1,10 @@
 'use strict';
 var dogFactory = require('../../app_test.js');
 var name = 'oggy',
-        doggy = dogFactory(name);
+    doggy;
+beforeEach(function() {
+  doggy = dogFactory(name);
+});
 describe("A factory", function() {
   it("constructor", function() {
     expect(doggy.name).toBe(name);
