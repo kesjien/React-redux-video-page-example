463a398d858164a98d43d50a2b155b96da4193d9
kes <glkes@yahoo.com>
2016-04-13 22:32:27 +0300

    id was added

--- client/components/MainSection/index.js
+++ client/components/MainSection/index.js
@@ -9,13 +9,13 @@ class App extends Component {
       <section>
         <div className={style.mainBox}>
           {this.props.videos.map(video =>
-            <div className={style.videoItem}>
+            <div key={video.id} className={style.videoItem}>
               <Video service={video.service} video={video.video} />
               <div className={style.infoBox}>
                 <div className={style.commentForm}> Add comments ...</div>
                 <div className={style.view}>Views: 0</div>
                 <a href="#"
-                  onClick={() => deleteVideo(video.video)}
+                  onClick={() => deleteVideo(video.id)}
                   videoId={video.video}
                   className={style.iconDelete}>
                   Delete
--- client/reducers/videos.js
+++ client/reducers/videos.js
@@ -3,26 +3,32 @@ import { handleActions } from 'redux-actions'
 
 const videos = [
   {
+    id: 0,
     service: 'youtube',
     video: 'XxVg_s8xAms',
   },
   {
+    id: 1,
     service: 'youtube',
     video: 'XuZLtMrCOoU',
   },
   {
+    id: 2,
     service: 'vimeo',
     video: '151715092',
   },
   {
+    id: 3,
     service: 'vimeo',
     video: '148177148',
   },
   {
+    id: 4,
     service: 'vimeo',
     video: '151715092',
   },
   {
+    id: 5,
     service: 'vimeo',
     video: '148177148',
   },
@@ -31,16 +37,22 @@ const videos = [
 
 export default handleActions({
   'add video' (state, action) {
-    const service = action.payload.split('.')[1];
-    const video = action.payload.split('v=')[1];
+    const service = (action.payload.split('.')[1] === 'youtube')
+      ? 'youtube'
+      : 'vimeo';
+    const video = (service === 'youtube')
+      ? action.payload.split('v=')[1]
+      : action.payload.split('com/')[1];
     return [{
       service: service,
-      video: video
+      video: video,
+      id: state.length,
     }, ...state]
   },
 
   'delete video' (state, action) {
-    return state.filter(video => video.video !== action.payload )
+    console.log(action)
+    return state.filter(video => video.id !== action.payload )
   },
 
   'edit video' (state, action) {
